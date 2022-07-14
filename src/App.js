7b4a7b4bffbd8379da5b00a0b04a8d68f85afe54
yusufgozukara <yusufgozukara11@gmail.com>
2022-07-14 13:52:19 +0300

    project finished

--- src/App.js
+++ src/App.js
@@ -43,6 +43,17 @@ if(loading){
   )
 }
 
+if(tours.length === 0){
+  return (
+    <main>
+      <div className="title">
+        <h2>No Tours Left</h2>
+        <button onClick={()=> fetchTours()} className='btn'>Refresh</button>
+      </div>
+    </main>
+  )
+}
+
   return (<main>
     <Tours tours={tours} removeTour={removeTour}/>
   </main>
